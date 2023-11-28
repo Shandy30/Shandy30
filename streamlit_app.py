560452c64c4c59be69d1451ae214c018a9e3dc4a
Shandy30 <151129658+Shandy30@users.noreply.github.com>
2023-11-28 21:32:17 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,17 +20,19 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+
 streamlit.header("Fruityvice Fruit Advice!")
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+   if not fruit_choice:
+      treamlit.error('Please select a fruit to get information.')
+   else 
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) # this line creates tabular object to the screen
+      streamlit.dataframe(fruityvice_normalized) #populates data inside the table on the screen
+except URLError as e:
+ streamlit.error()
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-# import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json()) #just writes data to the screen
-
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) # this line creates tabular object to the screen
-streamlit.dataframe(fruityvice_normalized) #populates data inside the table on the screen
 # do not run anything past here while we troubleshoot
 streamlit.stop()
 
